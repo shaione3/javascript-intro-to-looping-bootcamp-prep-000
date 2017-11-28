463070189a9095ac579a4c718d5dbb523bff7923
Nik <shaione3@gmail.com>
2017-11-28 05:02:37 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,11 @@
+function forLoop(array){
+  for(let i = 0; i < 25; i++){
+    if (i == 1){
+      array = "I am ${i} strange loop."
+      return array;
+    } else {
+      array = "I am ${i} strange loops."
+      return array;
+    }
+  }
+}
